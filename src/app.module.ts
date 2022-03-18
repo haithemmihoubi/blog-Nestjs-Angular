c99678c845ac8f6b0afe670f7d2c7d95853c1049
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-18 15:19:09 +0100

    genrating user resource crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      host: 'localhost',
+      username: 'postgres',
+      password: 'admin',
+      database: 'blog',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
