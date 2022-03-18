63ced940f9a543116d8121e78a58d0be32cb321c
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-18 21:20:46 +0100

    implementing jwt functionality in the authModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1000s' },
+      }),
+    }),
+  ],
+})
+export class AuthModule {}
